b94631f2f6b0a4f425055a1e5155eca7a182918c
FeiMa <mafei1992@outlook.com>
2024-05-22 16:29:54 +0200

    Add 'FileTable_Bpod_RunWheel.m' for wheel running analysis

--- WFanalysis/Matlab/Bpod/FileTable_Bpod_RunWheel.m
+++ WFanalysis/Matlab/Bpod/FileTable_Bpod_RunWheel.m
@@ -5,21 +5,110 @@ classdef FileTable_Bpod_RunWheel < FileTable_Bpod
     methods
         function obj = FileTable_Bpod_RunWheel()
             obj = obj@FileTable_Bpod('F:\users\Fei\Bpod\', 'm237X');
-            
+            obj.LoadFile().Plot();
         end
         
-        function LoadFile(obj)
+        function obj = LoadFile(obj)
+            % load data from mat files
             LoadFile@FileTable_Bpod(obj)
             obj.fileTable = loadDataBpodRunWheel(obj.fileTable);
             obj.fileTable = expendColumn(obj.fileTable, 'data');
+            % check and set threshold for wheel run duration
+            wheelRunSatrtEndTimes = cell2mat(obj.fileTable.wheelRunStartEndTimes);
+            wheelRunDuration = wheelRunSatrtEndTimes(:, 2) - wheelRunSatrtEndTimes(:, 1);
+            wheelRunDuration = rmoutliers(wheelRunDuration);
+            figure
+            histogram(wheelRunDuration, 'Normalization', 'probability');
+            axis tight;
+            xlabel('Time(s)');
+            ylabel('Probability');
+            title('Probalility distribution of wheel run duration for one trun');
+            lowThreshold = 0.2;
+            highThreshold = 1;
+            rectangle('Position', [lowThreshold, 0, highThreshold - lowThreshold, gca().YLim(2)], 'EdgeColor', 'r', 'LineStyle', '--', 'LineWidth', 1);
+            % filter out wheel run duration that are too short or too long
+            for i = 1:height(obj.fileTable)
+                wheelRunStartEndTimes = obj.fileTable.wheelRunStartEndTimes{i};
+                if isempty(wheelRunStartEndTimes)
+                    continue;
+                end
+                wheelRunDuration = wheelRunStartEndTimes(:, 2) - wheelRunStartEndTimes(:, 1);
+                idx = (wheelRunDuration >= lowThreshold) & (wheelRunDuration <= highThreshold);
+                obj.fileTable.wheelRunStartEndTimes{i} = wheelRunStartEndTimes(idx, :);
+                obj.fileTable.wheelRunDuration{i} = wheelRunDuration(idx);
+                obj.fileTable.wheelRunTurns{i} = sum(idx);
+            end
         end
 
-        function FilterWheelRun(obj)
-            wheelRunSatrtEndTimes = cell2mat(obj.fileTable.wheelRunStartEndTimes);
-            wheelRunSatrtEndTimes = wheelRunSatrtEndTimes(~isnan(wheelRunSatrtEndTimes(:, 1)), :);
-            wheelRunDuration = wheelRunSatrtEndTimes(:, 2) - wheelRunSatrtEndTimes(:, 1);
-            % remove outliers of wheelRunDuration with 3 std
-            wheelRunDuration = rmoutliers(wheelRunDuration);
+        function obj = Plot(obj)
+            % plot the number of wheel turns for each session
+            [gIdx, gTable] = findgroups(obj.fileTable(:, {'mouse', 'session'}));
+
+            for i = 1:height(gTable)
+                sessionData = obj.fileTable(gIdx == i, :);
+                trialNum = height(sessionData);
+                ropePullNum = sum(sessionData.ropePullNum);
+                wheelRunTurnNum = sum(cell2mat(sessionData.wheelRunTurns));
+                wheelRunDist = wheelRunTurnNum * pi * 13 / 100000; % 13cm is the diameter of the wheel, convert to km
+                wheelRunDuration = sum(cell2mat(sessionData.wheelRunDuration))/(60*60); % convert to hours
+                wheelRunSpeed = wheelRunDist / wheelRunDuration; % km/h
+
+                gTable.trialNum{i} = trialNum;
+                gTable.ropePullNum{i} = ropePullNum;
+                gTable.wheelRunDist{i} = wheelRunDist;
+                gTable.wheelRunDuration{i} = wheelRunDuration;
+                gTable.wheelRunSpeed{i} = wheelRunSpeed;
+            end
+            % remove sessions that have no wheel turns
+            gTable(cell2mat(gTable.wheelRunDist) == 0, :) = [];
+            % remove the first 5 sessions where the hardware was not working properly yet
+            gTable(1:4, :) = [];
+            nDays = height(gTable) + 4*2; % 4 weeks of weekends
+            nMouse = 5;
+            avgRopePullNum = sum(cell2mat(gTable.ropePullNum)) / nDays / nMouse;
+            avgWheelRunDist = sum(cell2mat(gTable.wheelRunDist)) / nDays / nMouse;
+            avgWheelRunDuration = sum(cell2mat(gTable.wheelRunDuration)) / nDays / nMouse;
+            avgWheelRunSpeed = mean(cell2mat(gTable.wheelRunSpeed));
+            
+            % plot results
+            figure('Color', 'w', 'Name', 'Wheel run analysis');
+            
+            subplot(2, 2, 1);
+            plot(1:height(gTable), cell2mat(gTable.ropePullNum), 'o-');
+            xticks(1:height(gTable));
+            xticklabels(gTable.session);
+            xtickangle(45);
+            xlabel('Session');
+            ylabel('Number of rope pulls');
+            title(sprintf('Rope pull = %.2f (per mouse, per day)', avgRopePullNum));
+            
+            subplot(2, 2, 2);
+            plot(1:height(gTable), cell2mat(gTable.wheelRunDuration), 'o-');
+            xticks(1:height(gTable));
+            xticklabels(gTable.session);
+            xtickangle(45);
+            xlabel('Session');
+            ylabel('Duration (h)');
+            title(sprintf('Duration = %.2fh (per mouse, per day)', avgWheelRunDuration));
+
+            subplot(2, 2, 3);
+            plot(1:height(gTable), cell2mat(gTable.wheelRunDist), 'o-');
+            xticks(1:height(gTable));
+            xticklabels(gTable.session);
+            xtickangle(45);
+            xlabel('Session');
+            ylabel('Distance (km)');
+            title(sprintf('Distance = %.2fkm (per mouse, per day)', avgWheelRunDist));
+
+            subplot(2, 2, 4);
+            plot(1:height(gTable), cell2mat(gTable.wheelRunSpeed), 'o-');
+            xticks(1:height(gTable));
+            xticklabels(gTable.session);
+            xtickangle(45);
+            xlabel('Session');
+            ylabel('Speed (km/h)');
+            title(sprintf('Speed = %.2fkm/h', avgWheelRunSpeed));
+            ylim([0, gca().YLim(2)]);
 
         end
     end
--- WFanalysis/Matlab/Bpod/loadDataBpodRunWheel.m
+++ WFanalysis/Matlab/Bpod/loadDataBpodRunWheel.m
@@ -34,96 +34,46 @@ function trialInfo = loadDataBpodRunWheelHelper(data)
 
         trialInfo{i, 2} = sum([ropePull1Num, ropePull2Num, ropePull3Num]);
 
-        if any(ismember(eventList, 'Wire1High')) && any(ismember(eventList, 'Wire1Low'))
-            wire1LowTimes = data.RawEvents.Trial{i}.Events.Wire1Low;
-            wire1LowTimes = reshape(wire1LowTimes, length(wire1LowTimes), 1);
+        wheelRunStartEndTimes = [];
+
+        % wheel 1, Wire1High
+        if any(ismember(eventList, 'Wire1High')) 
             wire1HighTimes = data.RawEvents.Trial{i}.Events.Wire1High;
-            wire1HighTimes = reshape(wire1HighTimes, length(wire1HighTimes), 1);
-
-            % pair the high and low times together
-            if (wire1HighTimes(1) < wire1LowTimes(1)) && (wire1HighTimes(end) > wire1LowTimes(end)) % high starts and high ends
-                wire1StartEndTimes = [wire1LowTimes, wire1HighTimes(2:end)];
-            elseif (wire1HighTimes(1) > wire1LowTimes(1)) && (wire1HighTimes(end) > wire1LowTimes(end)) % low starts and high ends
-                wire1StartEndTimes = [wire1LowTimes, wire1HighTimes];
-            elseif (wire1HighTimes(1) < wire1LowTimes(1)) && (wire1HighTimes(end) < wire1LowTimes(end)) % high starts and low ends
-                wire1StartEndTimes = [wire1LowTimes(1:end-1), wire1HighTimes(2:end)];
-            elseif (wire1HighTimes(1) > wire1LowTimes(1)) && (wire1HighTimes(end) < wire1LowTimes(end)) % low starts and low ends
-                wire1StartEndTimes = [wire1LowTimes(1:end-1), wire1HighTimes];
-            else
-                error('Wire1High and Wire1Low times are not paired correctly')
+            wire1HighTimes = reshape(wire1HighTimes, [], 1);
+            if length(wire1HighTimes) > 1
+                wheelRunStartEndTimes = [wheelRunStartEndTimes; wire1HighTimes(1:end-1), wire1HighTimes(2:end)]; %#ok<*AGROW>
             end
-        else
-            wire1StartEndTimes = [nan nan];
         end
 
-        if any(ismember(eventList, 'Wire2High')) && any(ismember(eventList, 'Wire2Low'))
-            wire2LowTimes = data.RawEvents.Trial{i}.Events.Wire2Low;
-            wire2LowTimes = reshape(wire2LowTimes, length(wire2LowTimes), 1);
+        % wheel 2, Wire2High
+        if any(ismember(eventList, 'Wire2High')) 
             wire2HighTimes = data.RawEvents.Trial{i}.Events.Wire2High;
-            wire2HighTimes = reshape(wire2HighTimes, length(wire2HighTimes), 1);
-
-            % pair the high and low times together
-            if (wire2HighTimes(1) < wire2LowTimes(1)) && (wire2HighTimes(end) > wire2LowTimes(end)) % high starts and high ends
-                wire2StartEndTimes = [wire2LowTimes, wire2HighTimes(2:end)];
-            elseif (wire2HighTimes(1) > wire2LowTimes(1)) && (wire2HighTimes(end) > wire2LowTimes(end)) % low starts and high ends
-                wire2StartEndTimes = [wire2LowTimes, wire2HighTimes];
-            elseif (wire2HighTimes(1) < wire2LowTimes(1)) && (wire2HighTimes(end) < wire2LowTimes(end)) % high starts and low ends
-                wire2StartEndTimes = [wire2LowTimes(1:end-1), wire2HighTimes(2:end)];
-            elseif (wire2HighTimes(1) > wire2LowTimes(1)) && (wire2HighTimes(end) < wire2LowTimes(end)) % low starts and low ends
-                wire2StartEndTimes = [wire2LowTimes(1:end-1), wire2HighTimes];
-            else
-                error('Wire2High and Wire2Low times are not paired correctly')
+            wire2HighTimes = reshape(wire2HighTimes, [], 1);
+            if length(wire2HighTimes) > 1
+                wheelRunStartEndTimes = [wheelRunStartEndTimes; wire2HighTimes(1:end-1), wire2HighTimes(2:end)];
             end
-        else
-            wire2StartEndTimes = [nan nan];
         end
 
-        if any(ismember(eventList, 'BNC1High')) && any(ismember(eventList, 'BNC1Low'))
-            bnc1LowTimes = data.RawEvents.Trial{i}.Events.BNC1Low;
-            bnc1LowTimes = reshape(bnc1LowTimes, length(bnc1LowTimes), 1);
-            bnc1HighTimes = data.RawEvents.Trial{i}.Events.BNC1High;
-            bnc1HighTimes = reshape(bnc1HighTimes, length(bnc1HighTimes), 1);
-
-            % pair the high and low times together
-            if (bnc1HighTimes(1) < bnc1LowTimes(1)) && (bnc1HighTimes(end) > bnc1LowTimes(end)) % high starts and high ends
-                bnc1StartEndTimes = [bnc1LowTimes, bnc1HighTimes(2:end)];
-            elseif (bnc1HighTimes(1) > bnc1LowTimes(1)) && (bnc1HighTimes(end) > bnc1LowTimes(end)) % low starts and high ends
-                bnc1StartEndTimes = [bnc1LowTimes, bnc1HighTimes];
-            elseif (bnc1HighTimes(1) < bnc1LowTimes(1)) && (bnc1HighTimes(end) < bnc1LowTimes(end)) % high starts and low ends
-                bnc1StartEndTimes = [bnc1LowTimes(1:end-1), bnc1HighTimes(2:end)];
-            elseif (bnc1HighTimes(1) > bnc1LowTimes(1)) && (bnc1HighTimes(end) < bnc1LowTimes(end)) % low starts and low ends
-                bnc1StartEndTimes = [bnc1LowTimes(1:end-1), bnc1HighTimes];
-            else
-                error('BNC1High and BNC1Low times are not paired correctly')
+        % wheel 3, BNC1High
+        if any(ismember(eventList, 'BNC1High')) 
+            BNC1HighTimes = data.RawEvents.Trial{i}.Events.BNC1High;
+            BNC1HighTimes = reshape(BNC1HighTimes, [], 1);
+            if length(BNC1HighTimes) > 1
+                wheelRunStartEndTimes = [wheelRunStartEndTimes; BNC1HighTimes(1:end-1), BNC1HighTimes(2:end)];
             end
-        else
-            bnc1StartEndTimes = [nan nan];
         end
 
-        if any(ismember(eventList, 'BNC2High')) && any(ismember(eventList, 'BNC2Low'))
-            bnc2LowTimes = data.RawEvents.Trial{i}.Events.BNC2Low;
-            bnc2LowTimes = reshape(bnc2LowTimes, length(bnc2LowTimes), 1);
-            bnc2HighTimes = data.RawEvents.Trial{i}.Events.BNC2High;
-            bnc2HighTimes = reshape(bnc2HighTimes, length(bnc2HighTimes), 1);
-
-            % pair the high and low times together
-            if (bnc2HighTimes(1) < bnc2LowTimes(1)) && (bnc2HighTimes(end) > bnc2LowTimes(end)) % high starts and high ends
-                bnc2StartEndTimes = [bnc2LowTimes, bnc2HighTimes(2:end)];
-            elseif (bnc2HighTimes(1) > bnc2LowTimes(1)) && (bnc2HighTimes(end) > bnc2LowTimes(end)) % low starts and high ends
-                bnc2StartEndTimes = [bnc2LowTimes, bnc2HighTimes];
-            elseif (bnc2HighTimes(1) < bnc2LowTimes(1)) && (bnc2HighTimes(end) < bnc2LowTimes(end)) % high starts and low ends
-                bnc2StartEndTimes = [bnc2LowTimes(1:end-1), bnc2HighTimes(2:end)];
-            elseif (bnc2HighTimes(1) > bnc2LowTimes(1)) && (bnc2HighTimes(end) < bnc2LowTimes(end)) % low starts and low ends
-                bnc2StartEndTimes = [bnc2LowTimes(1:end-1), bnc2HighTimes];
-            else
-                error('BNC2High and BNC2Low times are not paired correctly')
+        % wheel 4, BNC2High
+        if any(ismember(eventList, 'BNC2High')) 
+            BNC2HighTimes = data.RawEvents.Trial{i}.Events.BNC2High;
+            BNC2HighTimes = reshape(BNC2HighTimes, [], 1);
+            if length(BNC2HighTimes) > 1
+                wheelRunStartEndTimes = [wheelRunStartEndTimes; BNC2HighTimes(1:end-1), BNC2HighTimes(2:end)];
             end
-        else
-            bnc2StartEndTimes = [nan nan];
         end
-
-        trialInfo{i, 3} = [wire1StartEndTimes; wire2StartEndTimes; bnc1StartEndTimes; bnc2StartEndTimes];
         
+            trialInfo{i, 3} = wheelRunStartEndTimes;
     end
+
     trialInfo = cell2table(trialInfo, 'VariableNames', {'trialID', 'ropePullNum', 'wheelRunStartEndTimes'});
 end

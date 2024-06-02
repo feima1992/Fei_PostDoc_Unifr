791cccb8a53dd3813121b3696c6440e9c4d7f849
FeiMa <mafei1992@outlook.com>
2024-06-02 15:26:03 +0200

    Edit "WFS.m" for mannul check significant responsive cells

--- WFanalysis/Matlab/untitled3.m
+++
@@ -1,7 +0,0 @@
-for k = 1:size(trigData.sigCells,1)
-    plotDirTune(trigData, trigData.sigCells(k)) 
-    i = num2str(trigData.sigCells(k));
-    path='D:\Data\SingleCellData\New folder'; %Out folder
-    saveas(figure(1),fullfile(path,[ num2str(i) 'Out' '.jpeg'])); %save Out figures
-    close all
-end
